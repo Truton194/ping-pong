9832a9dcfdc1256f75a41b2755b6e350fcb06511
Truton194 <168847139+Truton194@users.noreply.github.com>
2024-05-05 19:16:47 +0300

    Add files via upload

--- ping pong.py
+++ ping pong.py
@@ -28,24 +28,52 @@ class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
         mouse_keys = mouse.get_pressed()
-        if keys[ K_u ] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[ K_d ] and self.rect.x < WIN_WIDTH - 695:
-            self.rect.x += self.speed
+        if keys[ K_w ] and self.rect.y >= 5:
+            self.rect.y -= self.speed
+        elif keys[ K_s ] and self.rect.y <= WIN_HEIGHT - 155:
+            self.rect.y += self.speed
 
     def update_l(self):
         keys = key.get_pressed()
         mouse_keys = mouse.get_pressed()
-        if keys[ K_w ] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[ K_s ] and self.rect.x < WIN_WIDTH - 695:
-            self.rect.x += self.speed
+        if keys[ K_UP ] and self.rect.y >= 5:
+            self.rect.y -= self.speed
+        if keys[ K_DOWN ] and self.rect.y <= WIN_HEIGHT - 155:
+            self.rect.y += self.speed
 
-player_1 = Player
-player_2 = Player
+class Ball(GameSprite):
+    def __init__(self, player_sprite, player_x, player_y, size_x, size_y, speed_x, speed_y):
+        super().__init__(player_sprite, player_x, player_y, size_x, size_y, speed_x)
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+
+    def update(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+        if self.rect.y >= WIN_HEIGHT:
+            self.speed_y *= -1
+
+        if self.rect.y <= 0:
+            self.speed_y *= -1
+
+    def collide_rect(self, player):
+        if self.rect.colliderect(player):
+            self.speed_x *= -1
+
+player_1 = Player('racket.png', 50, WIN_HEIGHT -280, 50, 150, 4)
+player_2 = Player('racket.png', WIN_WIDTH -100, WIN_HEIGHT - 280, 50, 150, 4)
+ball = Ball('tenis_ball.png', 275, WIN_HEIGHT - 280, 50, 50, 4, 4)
+
 
 game = True
 finish = False
+
+font.init()
+font_win = font.SysFont('Arial', 70)
+win_r = font_win.render('выйграл правый игрок', True, (255, 255, 255))
+win_l = font_win.render('выйграл левый игрок', True, (255, 255, 255))
+winner = None
 
 while game:
     for e in event.get():
@@ -55,5 +83,22 @@ while game:
     if not finish:
         window.fill(BG)
 
+        player_1.reset()
+        player_1.update_r()
+        player_2.reset()
+        player_2.update_l()
+        ball.reset()
+        ball.update()
+        ball.collide_rect(player_1)
+        ball.collide_rect(player_2)
+
+        if ball.rect.x < 50:
+            winner = win_r
+            finish = True
+        if ball.rect.x > WIN_WIDTH - 50:
+            winner = win_l
+            finish = True
+    elif finish:
+        window.blit(winner, (50, 200))
     display.update()
-    clock.tick(FPS)deactivate
+    clock.tick(FPS)
